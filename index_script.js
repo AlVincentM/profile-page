99031888e9269ad1ffcb2cffa90806d324155382
AlVincentM <alvincentviar.musa@bicol-u.edu.ph>
2019-12-28 15:33:58 +0800

    update!

--- index_script.js
+++ index_script.js
@@ -1,7 +1,24 @@
 $(document).ready(function() {
-	$("#about").scroll(function() {
-		$("#about").addClass(function() {
-			return "animate-fade-in";
-		});
-	});
+    
+    /* Every time the window is scrolled ... */
+    $(window).scroll( function(){
+    
+        /* Check the location of each desired element */
+        $('.about-text').each( function(i){
+            
+            var bottom_of_object = $(this).position().top + $(this).outerHeight();
+            var bottom_of_window = $(window).scrollTop() + $(window).height();
+            
+            /* If the object is completely visible in the window, fade it it */
+            if( bottom_of_window > bottom_of_object ){
+                
+                $(this).animate({'opacity':'1'},500);
+                console.log("animating");
+                    
+            }
+            
+        }); 
+    
+    });
+    
 });
